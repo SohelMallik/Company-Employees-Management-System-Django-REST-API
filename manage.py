7c1a92cc8cbfc03b882205428f89429675088285
Sohel Mallik <malliksohel582@gmail.com>
2026-09-10 21:28:56 +0530

    manage.py

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'companyapi.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'company_Employee_api.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
